4f9db40dac5fd5ce9682908fccc938b24ac9d770
George Chistyakov <chistopat@yandex-team.ru>
2019-01-04 20:54:06 +0300

    week02_02 fix with wraps from functools

--- course01/week2/week02_02.py
+++ course01/week2/week02_02.py
@@ -1,16 +1,22 @@
 import json
+from functools import wraps
 
 
 def to_json(func):
+    @wraps(func)
     def wrapped():
         return json.dumps(func())
     return wrapped
 
+# print(to_json.__name__)
+#
 # @to_json
 # def get_data():
 #   return {
 #     'data': 42
 #   }
 #
-# print(get_data())
+# print(get_data.__name__)
+# #
+# # print(get_data())
 
