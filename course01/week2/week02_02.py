ed1ae3a2cd795afa4366039d9e449f0e5612f118
George Chistyakov <chistopat@yandex-team.ru>
2019-01-04 21:02:37 +0300

    week02_02 fix args kwargs

--- course01/week2/week02_02.py
+++ course01/week2/week02_02.py
@@ -4,19 +4,19 @@ from functools import wraps
 
 def to_json(func):
     @wraps(func)
-    def wrapped():
+    def wrapped(*args, **kwargs):
         return json.dumps(func())
     return wrapped
 
-# print(to_json.__name__)
-#
+# # print(to_json.__name__)
+# #
 # @to_json
 # def get_data():
 #   return {
 #     'data': 42
 #   }
-#
-# print(get_data.__name__)
 # #
-# # print(get_data())
+# # print(get_data.__name__)
+# # #
+# print(get_data(1))
 
