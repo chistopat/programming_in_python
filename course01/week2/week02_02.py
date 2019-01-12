ad7c6ae46386956d4602b21a12feee6e6d43cf12
George Chistyakov <chistopat@yandex-team.ru>
2019-01-12 15:26:57 +0300

    week3

--- course01/week2/week02_02.py
+++ course01/week2/week02_02.py
@@ -5,18 +5,6 @@ from functools import wraps
 def to_json(func):
     @wraps(func)
     def wrapped(*args, **kwargs):
-        return json.dumps(func())
+        return json.dumps(func(*args, **kwargs))
     return wrapped
-
-# # print(to_json.__name__)
-# #
-# @to_json
-# def get_data():
-#   return {
-#     'data': 42
-#   }
-# #
-# # print(get_data.__name__)
-# # #
-# print(get_data(1))
 
